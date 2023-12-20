097d670f1d2de389e91c43fb9386f3d6af0869a6
VuBaHung <71269151+VuBaHung@users.noreply.github.com>
2023-12-20 22:29:05 +0700

    Update Timer.js

--- GameScreens/MathGames/Timer.js
+++ GameScreens/MathGames/Timer.js
@@ -1,1 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+export default function Timer({ time, gameOver, setGameOver }) {
+  const [timeLeft, setTimeLeft] = useState(time);
+  useEffect(() => {
+    let timer;
+    if (timeLeft > 0) {
+      timer = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      clearInterval(timer);
+      setGameOver(true);
+    }
 
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+  return (
+    <View>
+      <Text style={{ fontWeight: "bold", fontSize: 25, textAlign: "center" }}>
+        Time Remain: {timeLeft}s
+      </Text>
+    </View>
+  );
+}
