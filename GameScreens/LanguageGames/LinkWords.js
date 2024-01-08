97579ac561cd38c82066e5f5d654d3d23da01375
unknown <ducluandang22@gmail.com>
2024-01-08 18:43:18 +0700

    Add updates

--- GameScreens/LanguageGames/LinkWords.js
+++ GameScreens/LanguageGames/LinkWords.js
@@ -1,18 +1,152 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import GameTopBar from "../../components/GameTopBar";
 
-export default function LinkWords({ navigation: { goBack } }) {
+const words = [
+  "butter cup",
+  "cup cake",
+  "cake stand",
+  "stand up",
+  "up town",
+  "",
+];
+
+const LinkWords = ({ navigation: { goBack } }) => {
+  const initialWord = "butter";
+  const [data, setData] = useState(words);
+  const [inputWord, setInputWord] = useState("");
+  const [round, setRound] = useState(1);
+  const [previousInput, setPreviousInput] = useState("");
+  const [previousWord, setPreviousWord] = useState([]);
+  const [timer, setTimer] = useState(60); // Timer set to 60 seconds initially
+  const [points, setPoints] = useState(0); // Point counter
+
+  useEffect(() => {
+    let countdown;
+    if (timer > 0) {
+      countdown = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    } else {
+      clearInterval(countdown);
+      // Game over logic - show an alert
+      Alert.alert("Game Over", `Your final score: ${points}`, [
+        { text: "OK", onPress: () => goBack() },
+      ]);
+    }
+
+    return () => clearInterval(countdown);
+  }, [timer, points, goBack]);
+
+  const checkWord = () => {
+    const matchingWord = data.find(
+      (word) =>
+        word ===
+        (round === 1
+          ? initialWord + " " + inputWord
+          : previousInput + " " + inputWord)
+    );
+    if (matchingWord) {
+      setPoints((prevPoints) => prevPoints + 200); // Increment points on match
+      setPreviousInput(inputWord);
+      setData([...data.filter((word) => word !== matchingWord)]);
+      setInputWord("");
+      setPreviousWord((preState) => [
+        matchingWord.replace(/\s/g, ""),
+        ...preState,
+      ]);
+      setRound(round + 1);
+      setTimer(60); // Reset the timer to 30 seconds upon a correct match
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <GameTopBar goBack={goBack} />
-      <Text>LinkWords</Text>
+      <GameTopBar goBack={goBack} text={"Round " + round} />
+      <Text
+        style={{
+          fontSize: 18,
+          marginVertical: 10,
+          textAlign: "center",
+          fontWeight: "600",
+        }}
+      >
+        Time Remaining: {timer} seconds
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          marginVertical: 10,
+          textAlign: "center",
+          fontWeight: "600",
+        }}
+      >
+        Points: {points}
+      </Text>
+      <View style={styles.content}>
+        <Text style={styles.word}>
+          {round === 1 ? initialWord : previousInput} ____
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter a word"
+          value={inputWord}
+          onChangeText={(text) => setInputWord(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={checkWord}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+
+        <View style={{ alignItems: "center", gap: 5, marginTop: 30 }}>
+          <Text style={{ fontSize: 20, fontWeight: "600" }}>
+            {previousWord[0] ? "- Previous Word -" : ""}
+          </Text>
+          <Text style={{ fontSize: 18, fontWeight: "400" }}>
+            {previousWord[0] ? previousWord[0] : ""}
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
-}
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  content: {
+    alignItems: "center",
+  },
+  word: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    width: "80%",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
+
+export default LinkWords;
--- GameScreens/MemoryGames/FindMissingGame.js
+++ GameScreens/MemoryGames/FindMissingGame.js
@@ -1,179 +1,377 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
-import GameTopBar from "../../components/GameTopBar";
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import GameTopBar from "../../components/GameTopBar";
 
 export default function FindMissingGame({ navigation: { goBack } }) {
-  const [randomNumbers, setRandomNumbers] = useState([]);
-  const [questionMarkIndex, setQuestionMarkIndex] = useState(null);
-  const [countdown, setCountdown] = useState(15);
-  const [replaceDone, setReplaceDone] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [replacedNumber, setReplacedNumber] = useState(null);
-  const [countdownOver, setCountdownOver] = useState(false);
-  const [points, setPoint] = useState(0);
-  function generateUniqueRandomNumbers(number) {
-    const min = 1;
-    const max = 25;
-    const uniqueNumbers = new Set();
+  const images = [
+    require("../../assets/animal/images(1).jpeg"),
+    require("../../assets/animal/images(2).jpeg"),
+    require("../../assets/animal/images(3).jpeg"),
+    require("../../assets/animal/images(4).jpeg"),
+    require("../../assets/animal/images(5).jpeg"),
+    require("../../assets/animal/images(6).jpeg"),
+    require("../../assets/animal/images(7).jpeg"),
+    require("../../assets/animal/images(8).jpeg"),
+    require("../../assets/animal/images(9).jpeg"),
+    require("../../assets/animal/images(10).jpeg"),
+    require("../../assets/animal/images(11).jpeg"),
+    require("../../assets/animal/images(12).jpeg"),
+    require("../../assets/animal/images(13).jpeg"),
+    require("../../assets/animal/images(14).jpeg"),
+    require("../../assets/animal/images(15).jpeg"),
+    require("../../assets/animal/images(16).jpeg"),
+    require("../../assets/animal/images(17).jpeg"),
+    require("../../assets/animal/images(18).jpeg"),
+    require("../../assets/animal/images(19).jpeg"),
+    require("../../assets/animal/images(20).jpeg"),
+    require("../../assets/animal/images(21).jpeg"),
+    require("../../assets/animal/images(22).jpeg"),
+    require("../../assets/animal/images(23).jpeg"),
+  ];
 
-    while (uniqueNumbers.size < 3) {
-      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-      uniqueNumbers.add(randomNumber);
+  const [countdown, setCountdown] = useState(6);
+  const [secondCountdown, setSecondCountdown] = useState(15);
+  const [countdownFinished, setCountdownFinished] = useState(false);
+  const [secondCountdownFinished, setSecondCountdownFinished] = useState(false);
+  const [imageID, setImageID] = useState([]);
+  const [randomImageID, setRandomImageID] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [selectedOptions, setSelectedOption] = useState([]);
+  const [numberOfQuestionMark, setnumberOfQuestionMark] = useState(1);
+  const [round, setRound] = useState(1);
+  const [point, setPoint] = useState(0);
+  const [noOfRandomOptions, setNoOfRandomOptions] = useState(4);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [isTimeUp, setIsTimeUp] = useState(false);
+  function selectRandomElements(arr, count) {
+    if (arr.length < count) {
+      throw new Error(
+        "Array length should be at least the count of elements to select"
+      );
     }
 
-    const numbersArray = Array.from(uniqueNumbers);
+    const randomIndexes = new Set();
+    while (randomIndexes.size < count) {
+      randomIndexes.add(Math.floor(Math.random() * arr.length));
+    }
 
-    const randomIndex = Math.floor(Math.random() * (numbersArray.length + 1));
-    numbersArray.splice(randomIndex, 0, number);
+    const selectedElements = [...randomIndexes].map((index) => arr[index]);
+    return selectedElements;
+  }
 
-    return numbersArray;
+  function placeRandomly(arr1, numElements) {
+    const uniqueSet = new Set();
+
+    // Add unique elements from arr1 to the set
+    arr1.forEach((element) => uniqueSet.add(element));
+
+    // Add additional unique random numbers until reaching the desired count
+    while (uniqueSet.size < numElements) {
+      const randomNumber = Math.floor(Math.random() * 22) + 1;
+      uniqueSet.add(randomNumber);
+    }
+
+    // Convert the Set back to an array
+    const uniqueArray = Array.from(uniqueSet);
+
+    // Shuffle the array using Fisher-Yates algorithm
+    for (let i = uniqueArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [uniqueArray[i], uniqueArray[j]] = [uniqueArray[j], uniqueArray[i]];
+    }
+
+    return uniqueArray;
   }
 
   useEffect(() => {
-    generateRandomNumbers();
-  }, []);
+    if (countdownFinished === false) {
+      setImageID(generateRandomNumbers(5));
+      const timer1 = setTimeout(() => {
+        setCountdownFinished(true);
+      }, countdown * 1000);
+
+      return () => clearTimeout(timer1);
+    }
+    if (countdownFinished === true) {
+      setRandomImageID([
+        ...selectRandomElements(imageID, numberOfQuestionMark),
+      ]);
+    }
+  }, [countdownFinished, numberOfQuestionMark]);
 
   useEffect(() => {
-    let timeout;
-    let interval;
+    if (randomImageID.length > 0) {
+      setOptions([...placeRandomly(randomImageID, noOfRandomOptions)]);
+    }
+  }, [randomImageID]);
 
-    const startCountdown = () => {
-      timeout = setTimeout(() => {
-        const randomIndex = Math.floor(Math.random() * randomNumbers.length);
-        setQuestionMarkIndex(randomIndex);
-
-        const filteredOptions = randomNumbers.filter(
-          (_, index) => index !== randomIndex
-        );
-        const randomOptions = Array.from(
-          { length: 4 },
-          () =>
-            filteredOptions.splice(
-              Math.floor(Math.random() * filteredOptions.length),
-              1
-            )[0]
-        );
-        console.log(randomNumbers[randomIndex]);
-        randomOptions.push(randomNumbers[randomIndex]);
-        randomOptions.sort(() => Math.random() - 0.5);
-        setOptions(generateUniqueRandomNumbers(randomNumbers[randomIndex]));
-        setCountdownOver(true);
-      }, 15000);
-
-      interval = setInterval(() => {
-        setCountdown((prevCountdown) =>
+  useEffect(() => {
+    if (countdownFinished) {
+      const timer2 = setInterval(() => {
+        setSecondCountdown((prevCountdown) =>
           prevCountdown > 0 ? prevCountdown - 1 : 0
         );
       }, 1000);
-    };
 
-    if (randomNumbers.length > 0 && !replaceDone) {
-      startCountdown();
+      return () => {
+        clearInterval(timer2);
+        setSecondCountdownFinished(true);
+      };
     }
-
-    return () => {
-      clearTimeout(timeout);
-      clearInterval(interval);
-    };
-  }, [randomNumbers, replaceDone]);
+  }, [countdownFinished]);
 
   useEffect(() => {
-    if (questionMarkIndex !== null && !replaceDone) {
-      const updatedNumbers = [...randomNumbers];
-      updatedNumbers[questionMarkIndex] = "?";
-      setRandomNumbers(updatedNumbers);
-      setReplaceDone(true);
-      setReplacedNumber(randomNumbers[questionMarkIndex]);
-    }
-  }, [questionMarkIndex, randomNumbers, replaceDone]);
-
-  const generateRandomNumbers = () => {
-    const min = 1;
-    const max = 25;
-    const generatedNumbers = [];
-
-    for (let i = 0; i < 5; i++) {
-      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-      generatedNumbers.push(randomNumber);
-    }
-
-    setRandomNumbers(generatedNumbers);
-    setCountdown(15);
-    setQuestionMarkIndex(null);
-    setReplaceDone(false);
-    setOptions([]);
-    setReplacedNumber(null);
-    setCountdownOver(false);
-  };
-
-  const renderOptions = () => {
-    return options.map((option, index) => (
-      <TouchableOpacity key={index} onPress={() => handleOptionPress(option)}>
-        <Text style={styles.optionButton}>{option}</Text>
-      </TouchableOpacity>
-    ));
-  };
-
-  const handleOptionPress = (selectedOption) => {
-    let message = "";
-    let isCorrect = false;
-
-    if (parseInt(selectedOption) === replacedNumber) {
-      message = "Correct option selected!";
-      isCorrect = true;
+    if (secondCountdown === 0) {
+      setIsTimeUp(true);
     } else {
-      message = "Incorrect option selected!";
+      setIsTimeUp(false);
+    }
+    if (secondCountdown == 0 && round === 10) {
+      handleGameOver();
+      setRound(1);
+    }
+  }, [secondCountdown]);
+
+  useEffect(() => {
+    if (countdownFinished && isTimeUp) {
+      handleNext();
+    }
+  }, [countdownFinished, isTimeUp, isGameOver]);
+
+  const generateRandomNumbers = (count) => {
+    const min = 1;
+    const max = 22;
+    const generatedNumbers = new Set();
+
+    while (generatedNumbers.size < count) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      generatedNumbers.add(randomNumber);
     }
 
-    if (isCorrect) {
+    const uniqueNumbersArray = Array.from(generatedNumbers);
+    return uniqueNumbersArray;
+  };
+
+  useEffect(() => {
+    if (countdownFinished) {
+      if (round >= 1 && round <= 4) {
+        setnumberOfQuestionMark(1);
+        setNoOfRandomOptions(4);
+        setSecondCountdown(10);
+      } else if (round >= 5 && round <= 8) {
+        setnumberOfQuestionMark(2);
+        setNoOfRandomOptions(6);
+        setSecondCountdown(15);
+      } else if (round >= 9 && round <= 10) {
+        setnumberOfQuestionMark(3);
+        setNoOfRandomOptions(8);
+        setSecondCountdown(20);
+      }
+    }
+  }, [countdownFinished, round]);
+
+  const handleSelectImage = (option) => {
+    if (!selectedOptions.includes(option)) {
+      setSelectedOption((prevState) => [...prevState, option]);
+    } else {
+      setSelectedOption((prevState) => [
+        ...prevState.filter((e) => e !== option),
+      ]);
+    }
+  };
+
+  function handleNext() {
+    Alert.alert(
+      "Round " + round + " Is Over",
+      "Press 'Next' to move on",
+      [
+        {
+          text: "Next",
+          onPress: () => {
+            resetStates();
+            setRound(round + 1);
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+  function handleGameOver() {
+    setIsGameOver(true);
+    Alert.alert(
+      "Game Over",
+      "Would you like to try again?",
+      [
+        {
+          text: "Try Again",
+          onPress: () => {
+            resetStates();
+            setRound(1);
+            setIsGameOver(false);
+            setPoint(0);
+          },
+        },
+        {
+          text: "Exit",
+          onPress: () => {
+            goBack();
+          },
+          style: "cancel",
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+  const resetStates = () => {
+    setCountdown(6);
+    setSecondCountdown(15);
+    setCountdownFinished(false);
+    setSecondCountdownFinished(false);
+    setImageID(generateRandomNumbers(5));
+    setRandomImageID([]);
+    setOptions([]);
+    setSelectedOption([]);
+  };
+
+  const checkResults = () => {
+    const sortedRandomImageID = [...randomImageID].sort();
+    const sortedSelectedOptions = [...selectedOptions].sort();
+    const result =
+      JSON.stringify(sortedRandomImageID) ===
+      JSON.stringify(sortedSelectedOptions);
+
+    if (result && round < 11) {
+      setPoint(point + 200);
+      resetStates();
+      setSecondCountdownFinished(false);
+      handleNext();
+      setIsGameOver(false);
+    }
+
+    if (round === 10) {
+      // End the game here and display an alert
       Alert.alert(
-        "Correct",
-        "Press 'Next' to move on",
+        "Game Over",
+        `You've reached the final round. Your Score is ${point}. Would you like to try again?`,
         [
           {
-            text: "Next",
+            text: "Try Again",
             onPress: () => {
-              setPoint((preState) => preState + 200);
-              generateRandomNumbers();
+              resetStates();
+              setRound(1);
+              setIsGameOver(false);
+              setPoint(0);
             },
+          },
+          {
+            text: "Exit",
+            onPress: () => {
+              goBack();
+            },
+            style: "cancel",
           },
         ],
         { cancelable: false }
       );
-    } else {
-      Alert.alert("Incorrect", message);
     }
   };
 
+  const renderOptions = () => {
+    return options.map((option, index) => (
+      <TouchableOpacity key={index} onPress={(e) => handleSelectImage(option)}>
+        <Image
+          key={index}
+          style={{
+            height: 70,
+            width: 70,
+            resizeMode: "contain",
+            borderColor: "blue",
+            borderWidth: selectedOptions.includes(option) ? 1 : 0,
+          }}
+          source={images[option]}
+        />
+      </TouchableOpacity>
+    ));
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <GameTopBar goBack={goBack} />
+    <SafeAreaView>
+      <GameTopBar goBack={goBack} text={"Round " + round} />
       <View style={styles.numbersContainer}>
-        <Text style={styles.timerText}>Points: {points}</Text>
-        <Text style={styles.timerText}>Countdown: {countdown}</Text>
+        {!countdownFinished && (
+          <Text style={styles.timerText}> Remember These Images</Text>
+        )}
+        {countdownFinished && (
+          <Text style={styles.timerText}>
+            Time Remaining: {secondCountdown}
+          </Text>
+        )}
+        <Text style={{ fontSize: 20, marginBottom: 10 }}>Point: {point}</Text>
         <View style={styles.row}>
-          {randomNumbers.map((number, index) => (
-            <Text
-              key={index}
-              style={[
-                styles.text,
-                index === questionMarkIndex && styles.questionMark,
-              ]}
-            >
-              {number}
-            </Text>
-          ))}
+          {imageID.map((number, index) =>
+            randomImageID.includes(number) ? (
+              <Image
+                key={index}
+                style={{ height: 70, width: 70 }}
+                source={require("../../assets/animal/question-mark.png")}
+              />
+            ) : (
+              <Image
+                key={index}
+                style={{
+                  height: 70,
+                  width: 70,
+                  resizeMode: "contain",
+                }}
+                source={images[number]}
+              />
+            )
+          )}
         </View>
       </View>
+      {countdownFinished && (
+        <Text
+          style={{
+            marginTop: 20,
+            fontSize: 20,
+            fontWeight: "600",
+            textAlign: "center",
+          }}
+        >
+          Choose the Missing Images
+        </Text>
+      )}
       <View style={styles.optionsContainer}>
-        {countdownOver && options.length > 0 && renderOptions()}
+        {countdownFinished && options.length > 0 && (
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 2,
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            {renderOptions()}
+          </View>
+        )}
       </View>
-      {replacedNumber !== null && (
-        <View style={styles.replacedNumberContainer}>
-          <Text style={styles.replacedNumberText}>
-            Replaced Number: {replacedNumber}
-          </Text>
+
+      {countdownFinished && (
+        <View style={styles.centeredButtonContainer}>
+          <Button
+            style={styles.checkButton}
+            title="Check"
+            onPress={checkResults}
+          />
         </View>
       )}
     </SafeAreaView>
@@ -183,6 +381,18 @@ export default function FindMissingGame({ navigation: { goBack } }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f0f0f0", // Change to your preferred background color
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   numbersContainer: {
     marginBottom: 20,
@@ -194,19 +404,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexWrap: "wrap",
+    gap: 5,
   },
   text: {
-    fontSize: 18,
-    marginBottom: 10,
-    marginHorizontal: 5,
+    fontSize: 24,
+    marginBottom: 15,
+    marginHorizontal: 8,
+    fontFamily: "Roboto",
   },
   questionMark: {
     fontWeight: "bold",
     color: "red",
   },
   timerText: {
-    fontSize: 20,
-    marginBottom: 10,
+    fontSize: 22,
+    marginBottom: 15,
+    fontFamily: "Roboto",
+    fontWeight: "900",
   },
   optionsContainer: {
     flexDirection: "row",
@@ -215,12 +429,13 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   optionButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderWidth: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderWidth: 2,
     borderColor: "black",
-    borderRadius: 5,
-    fontSize: 16,
+    borderRadius: 8,
+    fontSize: 18,
+    fontFamily: "Roboto",
   },
   replacedNumberContainer: {
     alignItems: "center",
@@ -228,8 +443,22 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   replacedNumberText: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "bold",
     color: "blue",
+    fontFamily: "Roboto",
+  },
+  centeredButtonContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  checkButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    backgroundColor: "blue",
+    borderRadius: 8,
+    fontSize: 18,
+    fontFamily: "Roboto",
+    color: "white",
   },
 });
